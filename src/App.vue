9a4bef117a76f1b5ada8c89cbbe023f1f3091162
yangbingrui <1005401496@qq.com>
2021-11-20 21:33:40 +0800

    dist

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,14 @@
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
   components: {},
   methods: {
     test(vm) {
       // vm.onReload({duration: 1})
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style>
